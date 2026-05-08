12b8ec6b0048bb89ec543b7122a470449f37e310
Joschua <70809675+selfire1@users.noreply.github.com>
2026-05-08 12:06:41 +1000

    set up varlock

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next'
+import { varlockNextConfigPlugin } from '@varlock/nextjs-integration/plugin'
 
 const nextConfig: NextConfig = {
   images: {
@@ -7,4 +8,4 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+export default varlockNextConfigPlugin()(nextConfig)
